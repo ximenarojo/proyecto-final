ead87fa79d9c68859e330d73202c195e12f0d492
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 18:57:47 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -6,7 +6,6 @@ from PIL import Image
 
 st.markdown("<h1 style='text-align: center; color: black;'>SUNEDU:</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: black;'>Licenciamiento Institucional</h1>", unsafe_allow_html=True)
-st.markdown("##")
 st.markdown("---")
 st.header('¿Quiénes somos?')
 st.caption('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
