d35a8d7b9bcc3f2b56ed2d47089d0562d32cf8a6
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 18:42:07 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -6,11 +6,11 @@ from PIL import Image
 
 st.markdown("<h1 style='text-align: center; color: black;'>SUNEDU: Licenciamiento Institucional</h1>", unsafe_allow_html=True)
 st.markdown("##")
-st.markdown("<h1 style='text-align: center; color: black;'>Programación Avanzada - Proyecto 2022-2</h1>", unsafe_allow_html=True)
+st.header("<h1 style='text-align: center; color: black;'>Programación Avanzada - Proyecto 2022-2</h1>", unsafe_allow_html=True)
 st.markdown("---")
-st.markdown("##")
 st.header('¿Quiénes somos?')
-st.caption('Somos un grupo de estudiantes de 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por el curso de Programación Avanzada hemos creado ..')
+st.caption("<h1 style='color: black;'>Somos un grupo de estudiantes de 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por el curso de Programación Avanzada hemos creado</h1>", unsafe_allow_html=True))
 st.markdown("##")
 image = Image.open('integrantes.jpg')
 st.image(image)
+
