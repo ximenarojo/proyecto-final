a1b4c488640b05e3215637425cd07f3878c41b01
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 14:43:16 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -10,6 +10,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-df = pd.DataFrame(
- np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
- columns=['lat', 'lon'])
+st.title('SUNEDU: Licenciamiento Institucional')
+st.write(
+    """"**Bienvenido(a)**
+    En está sección podrás encontrar información sobre el avance y estatus del Licenciamiento Institucional de las universidades peruanas
+    """")
