8320c843d546638b3ad81e65aca3c5aa639940bb
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 18:34:26 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -2,18 +2,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 st.markdown("<h1 style='text-align: center; color: black;'>SUNEDU: Licenciamiento Institucional</h1>", unsafe_allow_html=True)
 st.markdown("##")
-
 st.markdown("<h1 style='text-align: center; color: black;'>Programación Avanzada - Proyecto 2022-2</h1>", unsafe_allow_html=True)
 st.markdown("---")
 st.markdown("##")
-
 st.header('¿Quiénes somos?')
 st.markdown("##")
 st.caption('Somos un grupo de estudiantes de 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia ...')
 st.markdown("##")
-
-from PIL import Image
 image = Image.open('borrador(1).jpg')
