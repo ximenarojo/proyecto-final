f9401564281e018a15927a38ec579cfc5d2cb164
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 18:23:03 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,14 +1,27 @@
-#import streamlit as st
-#import pandas as pd
-#import numpy as np
-#st.title('Titulo del proyecto')
-#st.write('**Bienvenido**')
-#n = st.slider('n',5,100,step=1)
-#chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
-#st.line_chart(chart_data)
-
+#$ pip install streamlit --upgrade
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('SUNEDU:Licenciamiento Institucional')
-st.write("**¡Bienvenido(a)!**")
+"""
+#------------------------------------------------------------------------------------------------------------
+# Bibliotecas
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import streamlit as st
+from datetime import datetime
+import matplotlib.pyplot as plt
+from PIL import Image
+
+#------------------------------------------------------------------------------------------------------------
+st.markdown("<h1 style='text-align: center; color: black;'>SUNEDU: Licenciamiento Institucional</h1>", unsafe_allow_html=True)
+st.markdown("##")
+
+st.markdown("<h1 style='text-align: center; color: black;'>Programación Avanzada - Proyecto 2022-2</h1>", unsafe_allow_html=True)
+st.markdown("---")
+st.markdown("##")
+
+st.header('¿Quiénes somos?')
+st.markdown("##")
+st.caption('Somos un grupo de estudiantes de 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia ...')
+st.markdown("##")
