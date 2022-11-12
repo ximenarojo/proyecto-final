291638ce1159b923ef493d2884851d429d74ffb3
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 15:29:47 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -7,19 +7,8 @@
 #chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
 #st.line_chart(chart_data)
 
-#import streamlit as st
-#import pandas as pd
-#import numpy as np
-#st.title('SUNEDU: Licenciamiento Institucional')
-#st.write("**Bienvenido(a)** En está sección podrás encontrar información sobre el avance y estatus del Licenciamiento Institucional de las universidades peruanas")
-<!DOCTYPE html>
-<html>
-<head>
-    <title>SUNEDU:Licenciamiento Institucional</title>
-</head>
-<body>
-    <h1>¡Bienvenido(a) a nuestra página web!</h1>
-    <p>**¿Quiénes somos?**</p>
-    <p>Somos un grupo de estudiantes cursando el 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia. En esta oportunidad, presentaremos el avance y estatus del Licenciamiento Institucional de las Universidades peruanas.  </p>
-</body>
-</html>
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('SUNEDU:Licenciamiento Institucional')
+st.write("**¡Bienvenido(a)!**")
