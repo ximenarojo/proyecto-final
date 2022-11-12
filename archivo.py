29ee04a42113734a6d7b1db71fa0bac1938828aa
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 14:17:33 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -2,13 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Titulo del proyecto')
-st.write('Hola **como** estas')
+st.write('**Bienvenido**')
 
 n = st.slider('n',5,100,step=1)
 chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(chart_data)
+st.map(chart_data)
 
-df = pd.DataFrame(
- np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
- columns=['latitud', 'longitud'])
-st.map(columns)
