532e2037d7d00692a3c803fb4631914b4cc540e2
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 14:02:40 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Titulo')
+st.title('Titulo del proyecto')
 n = st.slider('n',5,100,step=1)
 chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
