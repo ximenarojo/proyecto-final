83f63697fee97ca61c78c97f5548d7d9721f73f6
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 14:18:27 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,10 +1,15 @@
+#import streamlit as st
+#import pandas as pd
+#import numpy as np
+#st.title('Titulo del proyecto')
+#st.write('**Bienvenido**')
+#n = st.slider('n',5,100,step=1)
+#chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
+#st.line_chart(chart_data)
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Titulo del proyecto')
-st.write('**Bienvenido**')
-
-n = st.slider('n',5,100,step=1)
-chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
-st.map(chart_data)
-
+df = pd.DataFrame(
+ np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+ columns=['lat', 'lon'])
