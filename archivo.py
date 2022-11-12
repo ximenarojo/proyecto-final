0c1a68176737c8d08d041f4bf835523ff8d47aa2
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 18:55:47 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -9,7 +9,7 @@ st.markdown("<h1 style='text-align: center; color: black;'>Licenciamiento Instit
 st.markdown("##")
 st.markdown("---")
 st.header('¿Quiénes somos?')
-st.caption('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos creado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
+st.caption('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
 image = Image.open('integrantes.jpg')
 st.image(image)
-
+st.markdown("---")
