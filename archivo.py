13fae16f2fc920c1a9e76e259c88507131cf1c71
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-12 13:53:48 -0500

    Add files via upload

---
+++ archivo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+n = st.slider('n',5,100,step=1)
+chart_data= pd.DataFrame(np.random.randn(n),columns=['data'])
+st.line_chart(chart_data)
