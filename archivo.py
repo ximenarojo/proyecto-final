49cfe901a5f2595e18b5fadae514aca6c8a39b4f
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-14 22:33:00 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -43,8 +43,7 @@ if selected == 'Inicio':
 
 #--------------------------------------------------         
 if selected == 'Nosotros':
-             st.markdown("<h1 style='text-align: center; color: black;'>Nosotros:</h1>", unsafe_allow_html=True)
-             st.header('¿Quiénes somos?')
+             st.markdown("<h1 style='text-align: center; color: black;'>Equipo</h1>", unsafe_allow_html=True)
              st.write('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
              image = Image.open('integrantes.jpg')
              st.image(image)
