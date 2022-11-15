42df266cc1617b60cb67a6229b902ba8c9feb1a6
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-14 22:32:04 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -29,21 +29,20 @@ if selected == 'Inicio':
     st.image(image)
     st.write("**Fuente**: SUNEDU (2018). Disponible en: https://www.sunedu.gob.pe/8-condiciones-basicas-de-calidad/")
     st.markdown("##")
-    st.header('Etapas del Licenciamiento para universidades:')
+    st.header('Etapas del Licenciamiento para universidades')
     video_file = open('Etapas del Licenciamiento para las universidades.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes)
     st.write('**Fuente**: SUNEDU, 2015.')
     st.markdown("---")
     st.header("Conoce la lista completa de las universidades licenciadas en el Perú a la fecha (2022-09-01):")
-             
-             
-             
+
     st.markdown("---")
     st.caption("La información contenida en esta página web permite acceder al Dataset “Licenciamiento Institucional” elaborado por el Superintendencia Nacional de Educación Superior Universitaria (SUNEDU). Este ha registrado el avance y estatus del Licenciamiento Institucional de las universidades peruanas hasta el día 1 de septiembre de 2022.")
+    st.caption("Fuente de datos: https://www.datosabiertos.gob.pe/dataset/sunedu-licenciamiento-institucional")
 
 #--------------------------------------------------         
-if selected == 'Nostros':
+if selected == 'Nosotros':
              st.markdown("<h1 style='text-align: center; color: black;'>Nosotros:</h1>", unsafe_allow_html=True)
              st.header('¿Quiénes somos?')
              st.write('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
