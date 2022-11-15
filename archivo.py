1742b47cff37149a965d8252383fef9f6330986e
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-14 22:01:41 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -23,32 +23,30 @@ with st.sidebar:
 ###############################################################################
 ###############################################################################    
 if selected == 'Inicio':
-###############################################################################
-#ENCABEZADO DE LA PÁGINA#######################################################
-st.header('¿Qué es el Licenciamiento Institucional?')
-st.write("El Licenciamiento Institucional es un requisito obligatorio para todas las universidades del país, a través del cual cada casa de estudios debe demostrar ante la Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) que cumple con las Condiciones Básicas de Calidad (CBC) para poder brindar el servicio educativo. Como resultado de este proceso, existe un sistema universitario más ordenado y con una mayor orientación hacia la mejora continua (SUNEDU, 2018).")
-st.markdown("##")
-st.subheader("¿Qué es la SUNEDU?") 
-col1, col2 = st.columns(2)
-with col1:
-     st.write("La Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) es un organismo peruano adscrito al Ministerio de Educación con autonomía técnica, funcional, administrativa, económica y financiera, el cual se encarga de administrar el Registro Nacional de Grados y Títulos, bajo la consigna de brindar seguridad jurídica de la información que se encuentra registrada y garantizar su autenticidad. Asimismo, tiene como finalidad el licenciamiento, supervisión de la calidad, fiscalización del servicio educativo superior universitario.")
-with col2:
-    image= Image.open('SUNEDU.jpg')
+    st.header('¿Qué es el Licenciamiento Institucional?')
+    st.write("El Licenciamiento Institucional es un requisito obligatorio para todas las universidades del país, a través del cual cada casa de estudios debe demostrar ante la Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) que cumple con las Condiciones Básicas de Calidad (CBC) para poder brindar el servicio educativo. Como resultado de este proceso, existe un sistema universitario más ordenado y con una mayor orientación hacia la mejora continua (SUNEDU, 2018).")
+    st.markdown("##")
+    st.subheader("¿Qué es la SUNEDU?") 
+    col1, col2 = st.columns(2)
+     with col1:
+        st.write("La Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) es un organismo peruano adscrito al Ministerio de Educación con autonomía técnica, funcional, administrativa, económica y financiera, el cual se encarga de administrar el Registro Nacional de Grados y Títulos, bajo la consigna de brindar seguridad jurídica de la información que se encuentra registrada y garantizar su autenticidad. Asimismo, tiene como finalidad el licenciamiento, supervisión de la calidad, fiscalización del servicio educativo superior universitario.")
+     with col2:
+        image= Image.open('SUNEDU.jpg')
+        st.image(image)
+    st.markdown("##")
+    st.header('Condiciones Básicas de Calidad (CBC)')
+    st.write("Las Condiciones Básicas de Calidad (CBC) son un conjunto de estándares mínimos con los que la universidad debe contar para obtener el licenciamiento. Estos constituyen un mecanismo de protección a los estudiantes, sus familias y a la sociedad en su conjunto (SUNEDU, 2018).")
+    image = Image.open('CBC.jpeg')
     st.image(image)
-st.markdown("##")
-st.header('Condiciones Básicas de Calidad (CBC)')
-st.write("Las Condiciones Básicas de Calidad (CBC) son un conjunto de estándares mínimos con los que la universidad debe contar para obtener el licenciamiento. Estos constituyen un mecanismo de protección a los estudiantes, sus familias y a la sociedad en su conjunto (SUNEDU, 2018).")
-image = Image.open('CBC.jpeg')
-st.image(image)
-st.write("**Fuente**: SUNEDU (2018). Disponible en: https://www.sunedu.gob.pe/8-condiciones-basicas-de-calidad/")
-st.markdown("##")
-st.header('Etapas del Licenciamiento para universidades')
-video_file = open('Etapas del Licenciamiento para las universidades.mp4', 'rb')
-video_bytes = video_file.read()
-st.video(video_bytes)
-st.write("**Fuente**: SUNEDU. (2015). https://youtu.be/2NlkqlD7RTE&t=26s")   
-st.markdown("---")
-st.header("Conoce la lista completa de las universidades licenciadas en el Perú a la fecha (2022-09-01):")
+    st.write("**Fuente**: SUNEDU (2018). Disponible en: https://www.sunedu.gob.pe/8-condiciones-basicas-de-calidad/")
+    st.markdown("##")
+    st.header('Etapas del Licenciamiento para universidades')
+    video_file = open('Etapas del Licenciamiento para las universidades.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+    st.write("**Fuente**: SUNEDU. (2015). https://youtu.be/2NlkqlD7RTE&t=26s")   
+    st.markdown("---")
+    st.header("Conoce la lista completa de las universidades licenciadas en el Perú a la fecha (2022-09-01):")
 
 
 
@@ -58,9 +56,9 @@ st.header("Conoce la lista completa de las universidades licenciadas en el Perú
 
 ###############################################################################         
 if selected == 'Equipo':       
-st.header('¿Quiénes somos?')
-st.caption('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
-image = Image.open('integrantes.jpg')
-st.image(image)
-st.markdown("---")
+    st.header('¿Quiénes somos?')
+    st.caption('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
+    image = Image.open('integrantes.jpg')
+    st.image(image)
+    st.markdown("---")
 ############################################################################### 
