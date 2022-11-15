0f3d6721f5e8ca565d3eff2a9b85da5d635802f0
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-14 22:18:52 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -32,8 +32,7 @@ if selected == 'Inicio':
         image= Image.open('SUNEDU.jpg')
         st.image(image)
     st.markdown("##")
-   
-st.header('Condiciones Básicas de Calidad')
+st.header('Condiciones Básicas de Calidad (CBC)')
     st.write("Las Condiciones Básicas de Calidad (CBC) son un conjunto de estándares mínimos con los que la universidad debe contar para obtener el licenciamiento. Estos constituyen un mecanismo de protección a los estudiantes, sus familias y a la sociedad en su conjunto (SUNEDU, 2018).")
     image = Image.open('CBC.jpeg')
     st.image(image)
@@ -54,7 +53,7 @@ st.header('Condiciones Básicas de Calidad')
 #st.write("La información contenida en esta página web permite acceder al Dataset “Licenciamiento Institucional” elaborado por el Superintendencia Nacional de Educación Superior Universitaria (SUNEDU). Este ha registrado el avance y estatus del Licenciamiento Institucional de las universidades peruanas hasta el día 1 de septiembre de 2022.")
 
 ###############################################################################         
-if selected == 'Equipo':       
+if selected == 'Nostros':       
     st.header('¿Quiénes somos?')
     st.caption('Somos un grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado un dashboard para el análisis, visualización y exploración práctica e interactiva de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
     image = Image.open('integrantes.jpg')
