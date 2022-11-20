3fdc7e55373a01c92a92311579e3d83a0d9b9938
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-20 15:55:45 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -37,8 +37,7 @@ if selected == 'Inicio':
     st.video(video_bytes)
     st.write('**Fuente**: SUNEDU, 2015.')
     st.markdown("---")
-    st.subheader("Avance y estatus del Licenciamiento Institucional")
-    st.write('Dataset:')
+    st.header("Avance y estatus del Licenciamiento Institucional:")
     @st.experimental_memo
     def download_data():
         url ="https://raw.githubusercontent.com/ximenarojo/prueba/main/Licenciamiento%20Institucional_2.csv"
