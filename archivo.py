d248f539637184571f4406778a008c8b7c1dfe53
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-20 15:26:37 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -50,7 +50,7 @@ if selected == 'Inicio':
     download_data()
     #x=download_data()
     #st.write('Dimensiones: ' + str(x.shape[0]) + ' filas y ' + str(x.shape[1]) + ' columnas')
-    #st.dataframe(x)
+    st.dataframe(df)
     
 
     #st.markdown("---")
