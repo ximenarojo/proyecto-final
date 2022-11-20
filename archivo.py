9e34561913e389e2ae3d9edc3ccb8017b4fc214d
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-20 13:17:41 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -4,14 +4,12 @@ import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
 from PIL import Image
-import gdown
-import os
 
 
 #---------------------------------------------------------
 with st.sidebar:
     selected = option_menu(
-        menu_title= 'Menú',
+        #menu_title= 'Menú',
         options = ['Inicio', 'Reporte 2022', 'Nosotros'],
         icons=['house','book','people'],
         default_index=0,
@@ -46,38 +44,8 @@ if selected == 'Inicio':
     #Descargar y leer el archivo y considerar las comas como separadores
     datos = pd.read_csv(url, sep=',')
     st.line_chart(data=datos, x='NOMBRE', y='ESTADO_LICENCIAMIENTO')
-    
-    
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
-    
-    @st.experimental_memo
-    def download_data():
-        #https://drive.google.com/uc?id=
-        url = "https://drive.google.com/uc?id=1omgQxUg83yYExsGOaI__jdoT44O46Yfv"
-        output = "downloads/data.csv"
-        gdown.download(url,output,quiet = False)
-    
-    download_data()
-    df = pd.read_csv("downloads/data.csv", sep = ",", parse_dates = ["FECHA_CORTE","FECHA_RESULTADO"])
-    #Simplificacion del dataset (retiro de columnas)
-    df = df.drop(columns = ["FECHA_CORTE","FECHA_RESULTADO","UBIGEO","id_persona"])
-    
-    #Sistema de filtros
-    #Construccion del set/list de departamentos (Valores unicos sin NA)
-    set_departamentos = np.sort(df['DEPARTAMENTO'].dropna().unique())
-    #Seleccion del departamento
-    opcion_departamento = st.selectbox('Selecciona un departamento', set_departamentos)
-    df_departamentos = df[df['DEPARTAMENTO'] == opcion_departamento]
-    num_filas = len(df_departamentos.axes[0]) 
-    #Construccion del set/list de provincias (Valores unicos sin NA)
-    set_provincias = np.sort(df_departamentos['PROVINCIA'].dropna().unique())#Seleccion de la provincia
-    opcion_provincia = st.selectbox('Selecciona una provincia', set_provincias)
-    df_provincias = df_departamentos[df_departamentos['PROVINCIA'] == opcion_provincia]
-    num_filas = len(df_provincias.axes[0]) 
-    
-    st.write('Numero de registros:', num_filas)
-   
+  
+
 
     #st.markdown("---")
     st.caption("La información contenida en esta página web permite acceder al Dataset “Licenciamiento Institucional” elaborado por el Superintendencia Nacional de Educación Superior Universitaria (SUNEDU). Este ha registrado el avance y estatus del Licenciamiento Institucional de las universidades peruanas hasta el día 1 de septiembre de 2022.")
