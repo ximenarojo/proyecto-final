7171e16fc9c4c538cacb5acd3a955ae8798b7a2c
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-20 23:59:01 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -105,14 +105,14 @@ if selected == 'Inicio':
     st.bar_chart(bar_chart)
 #-------------------------------------------------- 
 if selected == 'Equipo':
-    st.markdown("<h1 style='text-align: center'>Integrante</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center'>¿Quiénes somos?</h1>", unsafe_allow_html=True)
     st.markdown("---")
-    st.write('Estudiante de 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivada por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, he desarrollado una app interactiva para la visualización y exploración práctica de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
+    st.write('Somos un grupo de estudiantes de 5to ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruana Cayetano Heredia, que motivados por los conocimientos adquiridos por el curso de Programación Avanzada y junto a la asesoría de los profesores, hemos desarrollado una app interactiva para la visualización y exploración práctica de los datos recopilados sobre el avance y estatus actual del Licenciamiento Institucional de las Universidades tanto públicas como privadas del Perú.')
     st.markdown("###")
-    #image = Image.open('integrantes.jpg')
-    #st.image(image)
-    #st.write('Correo de contacto:')
-    #st.write('- richerd.chavez@upch.pe')
-    #st.write('- isai.damazo@upch.pe')
+    image = Image.open('integrantes.jpg')
+    st.image(image)
+    st.write('Correo de contacto:')
+    st.write('- richerd.chavez@upch.pe')
+    st.write('- isai.damazo@upch.pe')
     st.write('- ximena.rojo@upch.pe')
 #---------------------------------------------------
