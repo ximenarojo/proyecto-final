0c10c70c94f9f94339bee7a70d6a7f324c8e8051
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-21 00:14:53 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -19,7 +19,7 @@ with st.sidebar:
         menu_title = 'Menú',
         options = ['Inicio', 'Ubicación','Reportes','Equipo'],
         icons = ['house', 'map', 'book','people'],
-        menu_icon='hand',
+        menu_icon='cast',
         default_index = 0,
     )
 #--------------------------------------------------------- 
