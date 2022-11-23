678e5a0e0c573efd1324ea3917739b3ab5ce6915
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 01:36:13 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -18,7 +18,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('upch.css') as f:
     st.markdown(f'<style>{f.read()}</upch>', unsafe_allow_html=True)
 with st.sidebar: 
-    st.sidebar.header('Proyecto Final 2022-2')
+    st.sidebar.header('Programación Avanzada - Proyecto Final 2022-2')
     st.write('Análisis y exploración de datos sobre el avance y estatus del Licenciamiento Institucional de las universidades peruanas.')
     selected = option_menu(
         menu_title = 'Menú',
@@ -26,7 +26,14 @@ with st.sidebar:
         icons = ['house', 'map', 'book','people'],
         menu_icon='cast',
         default_index = 0,
-    )    
+    )
+    st.sidebar.markdown('''
+    ---
+    Creado por Ximena Rojo en
+    ''')
+    with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
 #--------------------------------------------------------- 
 if selected == 'Inicio':
     st.markdown("<h1 style ='text-align: center'>SUNEDU:</h1>", unsafe_allow_html=True)
