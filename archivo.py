5abaa7c90838f7a6fcc49632aad4fa1a76c35458
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 09:23:40 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -154,7 +154,7 @@ if selected == 'Localización':
         opcion = 'licencia otorgada'
         st.markdown("###")
         st.write('**Gráfico 3. Universidades con '+opcion+' localizadas en un mapa interactivo mundial.**')
-        map = folium.Map(location=[-9.19, -75])
+        map = folium.Map(location=[-9.19, -74], zoom_start=4)
         st_map = st_folium(map, width=800, height=450)
         st.markdown("###")
         st.write('**Lista de universidades con '+opcion+' localizadas en un mapa interactivo mundial.**')
