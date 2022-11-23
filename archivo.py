e4d3792d113d69b14febd8a01120c2c82a910619
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 01:01:37 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -15,8 +15,7 @@ from PIL import Image
 
 #---------------------------------------------------------
 with st.sidebar:
-    st.sidebar.header('Programación Avanzada')
-    st.sidebar.header('`Proyecto Final 2022-2`')
+    st.sidebar.header('Programación Avanzada Proyecto Final `2022-2`')
     selected = option_menu(
         menu_title = 'Menú',
         options = ['Inicio', 'Localización','Reportes','Equipo'],
