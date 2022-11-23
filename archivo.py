d5c58211e947938e4125a34ecdb8e5e1242fd729
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 01:42:20 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -18,8 +18,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('upch.css') as f:
     st.markdown(f'<style>{f.read()}</upch>', unsafe_allow_html=True)
 with st.sidebar: 
-    #st.sidebar.header('Programación Avanzada - Proyecto Final 2022-2')
-    st.sidebar.header("<h1 style ='text-align: center'>Programación Avanzada - Proyecto Final 2022-2:</h1>", unsafe_allow_html=True)
+    st.sidebar.header('Programación Avanzada - Proyecto Final 2022-2')
     st.write('Análisis y exploración de datos sobre el avance y estatus del Licenciamiento Institucional de las universidades peruanas.')
     selected = option_menu(
         menu_title = 'Menú',
