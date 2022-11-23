9b5033ccb0194560a2f08055cb1301ed5bc3f8d3
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 00:37:47 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -15,6 +15,7 @@ from PIL import Image
 
 #---------------------------------------------------------
 with st.sidebar:
+    st.sidebar.header('Dashboard')
     selected = option_menu(
         menu_title = 'Menú',
         options = ['Inicio', 'Localización','Reportes','Equipo'],
