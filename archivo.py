c9a83d6b0180263423b7e186f77880b5ea94716b
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 01:32:14 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -26,11 +26,7 @@ with st.sidebar:
         icons = ['house', 'map', 'book','people'],
         menu_icon='cast',
         default_index = 0,
-    )
-    st.sidebar.markdown('''
-    ---
-    Creado por Ximena Rojo
-    ''')
+    )    
 #--------------------------------------------------------- 
 if selected == 'Inicio':
     st.markdown("<h1 style ='text-align: center'>SUNEDU:</h1>", unsafe_allow_html=True)
