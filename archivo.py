949c4b501660f50d69918c9d982ce7e6e7e46311
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-23 01:26:53 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -14,6 +14,7 @@ from PIL import Image
 #st.line_chart(data=datos, x='NOMBRE', y='ESTADO_LICENCIAMIENTO')
 
 #---------------------------------------------------------
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
