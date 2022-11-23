8a9fb9cbcb21524621ebb48c6a72985bdd893e76
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-22 22:28:47 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -112,9 +112,9 @@ if selected == 'Inicio':
     st.markdown("###")
     bar_chart = df_DISTRITO.TIPO_GESTION.value_counts()
     bar_chart = pd.DataFrame(bar_chart)
-    bar_chart.columns = ['TIPO DE GESTION']
+    bar_chart.columns = ['Tipo de gestión']
     st.write('**Gráfico 2. Tipo de gestión de las universidades según zona geográfica seleccionada.**')
-    st.caption('Donde "value" hace referencia a la cantidad de universidades registradas para el tipo de gestión,')
+    st.caption('Value hace referencia a la cantidad de universidades registradas para el tipo de gestión.')
     st.markdown("###")
     st.bar_chart(bar_chart)
     
