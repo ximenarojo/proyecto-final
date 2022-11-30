fb1abd26246c6a9e2c0f380c3b322719d932d2e8
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-30 15:23:38 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -225,8 +225,7 @@ if selected == 'Reportes':
     df = pd.read_csv('Licenciamiento%20Institucional_2.csv')
     periodo = df['PERIODO_LICENCIAMIENTO'].unique().tolist()
     st.write('**A continuación, seleccione solo un período de licenciamiento:**')
-    periodo_selec = st.multiselect('',
-                                   periodo,
+    periodo_selec = st.multiselect('', periodo,
                                    default = periodo)
     
     x = (df['PERIODO_LICENCIAMIENTO'].isin(periodo_selec))
