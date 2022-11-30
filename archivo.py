3a779a68dfd6ef59612de1d395cf31c48691ffc9
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-30 15:18:35 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -230,7 +230,7 @@ if selected == 'Reportes':
     
     mask = (df['PERIODO_LICENCIAMIENTO'].isin(periodo_selec))
     num = df[mask].shape[0]
-    st.write('*Se encontraron {num} resultados para su búsqueda.*')
+    st.write(f'Se encontraron {num} resultados para su búsqueda.')
     
         
     
