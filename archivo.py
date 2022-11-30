117813528a63d3693f32a354bb1574eafa928be5
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-30 15:02:28 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -225,9 +225,9 @@ if selected == 'Reportes':
     df = pd.read_csv('Licenciamiento%20Institucional_2.csv')
     periodo = df['PERIODO_LICENCIAMIENTO'].unique().tolist()
     periodo_selec = st.slider('A continuación, seleccione un período de licenciamiento:',
-                              min_value= min(df),
-                              max_value= max(df),
-                              value=(min(df),max(df)))
+                              min_value= min(periodo),
+                              max_value= max(periodo),
+                              value=(min(periodo),max(periodo)))
     
     
 
