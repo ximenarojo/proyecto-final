58583fc5250a47fc8716feec8b6d2a6bf3858b86
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-30 18:50:14 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -18,7 +18,7 @@ with st.sidebar:
     selected = option_menu(
         menu_title = 'Menú',
         options = ['Inicio', 'Localización','Vigencia','Equipo'],
-        icons = ['house', 'map', 'calendar','people'],
+        icons = ['house', 'map', 'time','people'],
         menu_icon='cast',
         default_index = 0,
         styles={
