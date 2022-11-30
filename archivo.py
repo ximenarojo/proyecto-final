bce459230fc21f49de5e81d38e7f58187d0c2962
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-11-30 14:51:34 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -222,6 +222,13 @@ if selected == 'Localización':
 if selected == 'Reportes':
     st.markdown("<h1 style ='text-align: center'>Períodos de Licenciamiento</h1>", unsafe_allow_html=True)
     st.markdown("---")
+    periodo = df['PERIODO_LICENCIAMIENTO'].unique().tolist()
+    periodo_selec = st.multiselect('Periodo:',
+                                    periodo,
+                                    default=periodo)
+
+    
+    
     
     
     
