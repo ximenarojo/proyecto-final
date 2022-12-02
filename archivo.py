8c8c42959dba4f6805c71e9a84a4aa661896245d
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-12-02 09:29:40 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,6 +1,6 @@
 #$ pip install streamlit --upgrade 
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import numpy as np
 from streamlit_option_menu import option_menu
 import urllib.request
