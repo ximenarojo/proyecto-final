dabb5d960f132316cf122cda92abfe41011f183c
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-12-03 15:25:38 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -243,7 +243,7 @@ if selected == 'Localización':
 if selected == 'Periodo':
     st.markdown("<h1 style ='text-align: center'>Vigencia del Licenciamiento Institucional</h1>", unsafe_allow_html=True)
     st.markdown("---")
-    st.write('La Licencia Institucional es de carácter temporal y renovable, otorgada o denegada por el Consejo Directivo de la SUNEDU. Esta se determina en base a una serie de factores, entre los que destaca la promoción de la investigación y los hallazgos que dicha universidad pueda exponer ante la comunidad internacional.')
+    st.write('La Licencia Institucional es de carácter temporal y renovable, otorgada o denegada por el Consejo Directivo de la SUNEDU. Esta se determina en base a una serie de factores, entre los que destaca la promoción de la investigación y los hallazgos que dicha universidad pueda exponer ante la comunidad internacional. Hasta la fecha (31/08/2022), **de las 93** universidades licenciadas, **solo 5** han sido beneficiadas con el tiempo máximo de licenciamiento.')
     st.write('- La Ley Universitaria estableció un periodo mínimo de vigencia de seis (6)años, al que se suma otro de ocho (8) y de diez(10) años.')
     st.markdown("###")
     df = pd.read_csv('Licenciadas.csv')
@@ -255,13 +255,12 @@ if selected == 'Periodo':
     ax1.pie(pie_chart['TOTAL'], labels = pie_chart['PERIODO_LICENCIAMIENTO'], autopct='%1.1f%%')
     ax1.axis('equal')
     st.write('**Gráfico 4.** Porcentaje de universidades con respecto al período de licenciamiento (en años).')
-    st.markdown("###")
     st.pyplot(fig1)
     
     
-    st.write('Hasta la fecha (31/08/2022), **de las 93** universidades licenciadas, **solo 5** han sido beneficiadas con el tiempo máximo de licenciamiento.')
+    
     image = Image.open('Vigencia.png')
-    st.image(image) 
+    #st.image(image) 
     st.write("**Fuente**: Elaboración propia.")
         
     st.markdown("###")
