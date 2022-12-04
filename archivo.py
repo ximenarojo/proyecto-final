52f46710997bbfe54e6ebe6662fefc5cadeaeeeb
ximenarojo <112133361+ximenarojo@users.noreply.github.com>
2022-12-03 20:31:46 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -245,7 +245,7 @@ if selected == 'Periodo':
     st.markdown("<h1 style ='text-align: center'>Períodos del Licenciamiento Institucional</h1>", unsafe_allow_html=True)
     st.markdown("---")
     st.write('La Licencia Institucional es de carácter temporal y renovable, otorgada o denegada por el Consejo Directivo de la SUNEDU. Esta se determina en base a una serie de factores, entre los que destaca la promoción de la investigación y los hallazgos que dicha universidad pueda exponer ante la comunidad internacional.')
-    st.write('La Ley Universitaria estableció un periodo mínimo de vigencia de seis (6)años, al que se suma otro de ocho (8) y de diez(10).')
+    st.write('La Ley Universitaria estableció un periodo mínimo de vigencia de seis (6)  años, al que se suma otro de ocho (8) y de diez(10).')
     st.markdown("###")
     #df = pd.read_csv('Licenciadas.csv')
     st.write('**Gráfico 4.** Cantidad de universidades por períodos de vigencia (en años).')
